3e1143196979ff664d23434ac25afc67ce246dff
1SaiVaruna <sai030804@gmail.com>
2024-10-02 19:41:04 +0100

    Added public enum 'GradingClassSystem'.

--- GradeCalculator/Program.cs
+++ GradeCalculator/Program.cs
@@ -2,6 +2,16 @@
 {
     internal class Program
     {
+
+        public enum GradingClassSystem
+        {
+            _1,
+            _21,
+            _22,
+            _3,
+            _U
+        }
+
         static void Main(string[] args)
         {
         }
