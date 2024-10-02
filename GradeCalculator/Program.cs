fa33cf04a54f215dde919395bb5c1a1d64d70564
1SaiVaruna <sai030804@gmail.com>
2024-10-02 19:45:12 +0100

    Added 'Module' class.

--- GradeCalculator/Program.cs
+++ GradeCalculator/Program.cs
@@ -12,6 +12,24 @@
             _U
         }
 
+        public class Module
+        {
+            public string ID { get; set; }
+            public string Title { get; set; }
+            public int Credits { get; set; }
+            public double Marks {  get; set; }
+
+            public Module(string id, string title, int credits, double marks)
+            {
+                ID = id;
+                Title = title;
+                Credits = credits;
+                Marks = marks;
+            }
+
+        }
+
+
         static void Main(string[] args)
         {
         }
