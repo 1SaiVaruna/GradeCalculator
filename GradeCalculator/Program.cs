e998ff84ddf8e0b523c2d62d262dd34b5c537ebf
1SaiVaruna <sai030804@gmail.com>
2024-10-02 20:20:46 +0100

    Added method to calculate 'NormalAverage'.

--- GradeCalculator/Program.cs
+++ GradeCalculator/Program.cs
@@ -34,12 +34,12 @@
         {
             List<Module> modules = new List<Module>();
             {
-                new Module("CET252", "Agile Development", 20, InputMarks("Agile Development")),
-                new Module("CET253", "Database Systems", 20, InputMarks("Databases Systems")),
-                new Module("CET254", "Advanced Programming", 20, InputMarks("Advanced Programming")),
-                new Module("CET255", "IoT and Robotics", 20, InputMarks("IoT and Robotics")),
-                new Module("CET256", "Cyber Security", 20, InputMarks("Cyber Security")),
-                new Module("CET257", "Enterprise Project", 20, InputMarks("Enterprise Project"))
+                new Module("CET252", "Agile Development", 20, InputMarks("Agile Development"));
+                new Module("CET253", "Database Systems", 20, InputMarks("Databases Systems"));
+                new Module("CET254", "Advanced Programming", 20, InputMarks("Advanced Programming"));
+                new Module("CET255", "IoT and Robotics", 20, InputMarks("IoT and Robotics"));
+                new Module("CET256", "Cyber Security", 20, InputMarks("Cyber Security"));
+                new Module("CET257", "Enterprise Project", 20, InputMarks("Enterprise Project"));
             };
 
             double averageMarks = CalculateNormalAverage(modules);
@@ -65,6 +65,21 @@
             {
                 Console.Write("INVALID INPUT. TRY AGAIN. ENTER MARKS ( 0 - 100 ): ");
             }
+
+        }
+
+        static double CalculateNormalAverage(List<Module> modules)
+        {
+            double totalMarks = 0;
+            int totalCredits = 0;
+
+            foreach (var module in modules)
+            {
+                totalMarks += module.Marks + module.Credits;
+                totalCredits += module.Credits;
+            }
+
+            return totalMarks / totalCredits;
 
         }
 
