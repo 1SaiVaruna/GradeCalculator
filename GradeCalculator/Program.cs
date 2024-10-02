ff81d7f6ba851c529f7520f8f364ae7c003dd63b
1SaiVaruna <sai030804@gmail.com>
2024-10-02 20:00:01 +0100

    Added modules and method to input marks.

--- GradeCalculator/Program.cs
+++ GradeCalculator/Program.cs
@@ -32,6 +32,28 @@
 
         static void Main(string[] args)
         {
+            List<Module> modules = new List<Module>();
+            {
+                new Module("CET252", "Agile Development", 20, InputMarks("Agile Development" ),
+                new Module("CET253", "Database Systems", 20, InputMarks("Databases Systems"),
+                new Module("CET254", "Advanced Programming", 20, InputMarks("Advanced Programming")),
+                new Module("CET255", "IoT and Robotics", 20, InputMarks("IoT and Robotics")),
+                new Module("CET256", "Cyber Security", 20, InputMarks("Cyber Security")),
+                new Module("CET257", "Enterprise Project", 20, InputMarks("Enterprise Project")),
+            };
+        }
+
+        static double InputMark(string moduleName)
+        {
+            double marks;
+
+            Console.Write($"ENTER MARKS FOR: {moduleName}: ");
+
+            while (!double.TryParse(Console.ReadLine(), out marks) || marks < 0 || marks > 100)
+            {
+                Console.Write("INVALID INPUT. TRY AGAIN. ENTER MARKS ( 0 - 100 ): ");
+            }
+
         }
     }
 }
